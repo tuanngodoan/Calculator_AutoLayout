4330fbea83ad325724ba1f95ce1d4ed01cb28840
Doãn Tuấn <tuanngodoan@hotmail.com>
2016-11-25 20:46:20 +0700

    Auto layout and design basic

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,14 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var distanceBetweenInputTextFieldAndResultLable: NSLayoutConstraint!
+    
+    @IBOutlet weak var distanceBetweenResultLableAndMiddleView: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func updateConstrains(){
+        
+        let scale = UIScreen.main.bounds.size.height / 667
+        
+        distanceBetweenInputTextFieldAndResultLable.constant = UIScreen.main.bounds.size.height > 480 ?
+            distanceBetweenInputTextFieldAndResultLable.constant * scale : distanceBetweenInputTextFieldAndResultLable.constant * 0.1
+        
+        distanceBetweenResultLableAndMiddleView.constant = UIScreen.main.bounds.size.height > 480 ?
+            distanceBetweenResultLableAndMiddleView.constant * scale : distanceBetweenResultLableAndMiddleView.constant * 0.1
     }
 
 
